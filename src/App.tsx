71df2b5e09fdc9299b4a2ec17be807931fd5be55
Johnny Pesola <johnny@pesola.eu>
2020-03-15 14:56:14 +0100

    Make bruno playable

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Card, { CardColor, CardValue } from './components/Card';
 import CardPile, { getRandomCard } from './components/CardPile';
+import Table from './components/Table';
+import Hand from './components/Hand';
 
 export interface CardInHand {
   color: CardColor;
@@ -20,18 +22,33 @@ function App() {
     return initialHand;
   }
 
-  const [hand, setHand] = useState<CardInHand[]>(getInitalHand());
+  const [cardsInHand, setCardsInHand] = useState<CardInHand[]>(getInitalHand());
+  const [topCard, setTopCard] = useState<CardInHand>(getRandomCard());
 
-  const onGetCardHandler = (newCard: CardInHand) => setHand([...hand, newCard])
+  const onGetCardHandler = (newCard: CardInHand) => setCardsInHand([...cardsInHand, newCard])
+
+  const placeCardFromHand = (cardInHand: CardInHand, cardIndex: number) => {
+
+    if (cardInHand.color == topCard.color || cardInHand.value == topCard.value){
+      setTopCard(cardInHand);
+      cardsInHand.splice(cardIndex, 1);  
+    }
+
+  }
 
   return (
     <>
-      <div><CardPile onGetCard={onGetCardHandler}/></div>
-      <div>
-        {hand.map(card => 
+      <Table>
+        <CardPile onGetCard={onGetCardHandler} />
+        <Card color={topCard.color} value={topCard.value} />
+      </Table>
+      <Hand>
+        {cardsInHand.map((card, index) => 
+          <span onClick={() => {placeCardFromHand(card, index)}}>
             <Card color={card.color} value={card.value} />
-          )}
-      </div>
+          </span>
+        )}
+      </Hand>
     </>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import { CardInHand } from '../App';
 
 
 export enum CardColor {
