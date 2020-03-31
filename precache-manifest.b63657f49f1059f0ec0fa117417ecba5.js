4978c08ee1f19f85a93a4fca74126e2642875485
Johnny Pesola <johnny@pesola.eu>
2020-04-01 00:20:40 +0200

    Updates

---
+++ precache-manifest.b63657f49f1059f0ec0fa117417ecba5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c0d23cb16b7a2fa9941eb20177fb14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d337517eabc5eb24acb",
+    "url": "/static/css/main.a120365b.chunk.css"
+  },
+  {
+    "revision": "34744d9e9a7554b59048",
+    "url": "/static/js/2.bd0314a5.chunk.js"
+  },
+  {
+    "revision": "e46a43ebdb77fba7de7da3971f7a747a",
+    "url": "/static/js/2.bd0314a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d337517eabc5eb24acb",
+    "url": "/static/js/main.509e0a6d.chunk.js"
+  },
+  {
+    "revision": "f18e1029f2424fff45a1",
+    "url": "/static/js/runtime-main.3d4f9d74.js"
+  }
+]);
